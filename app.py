feb615d17cc11b66a092a505fd66657880fc298d
RealAwesomeness <RealAwesomeness@outlook.com>
2020-07-20 16:46:54 -0400

    static files can be loaded by nginx

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ app.config['FAVICONS_DIRECTORY'] = './favicons'
 def index():
       return render_template("template.html", content="<b>soon there shall be content</b>")
 
-@app.route('/favicons/<path:favicon>')
-def favicons(favicon):
-      return send_from_directory(app.config['FAVICONS_DIRECTORY'], favicon, as_attachment=True)
 
 if __name__ == '__main__':
    app.run(debug = True)
